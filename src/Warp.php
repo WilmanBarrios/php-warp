c791ad9f4c863a49536345bb6cca331208b5c854
Wilman Barrios <wilmanbarrios@gmail.com>
2018-06-24 14:40:04 -0400

    Add some basic operations

--- src/Warp.php
+++ src/Warp.php
@@ -4,5 +4,98 @@ namespace Warp;
 
 class Warp
 {
-
+    static function __callStatic($method, $args)
+    {
+        return Space::new()->{$method}(...$args);
+    }
 }
+
+class Space
+{
+    static function new(...$args)
+    {
+        return new self(...$args);
+    }
+
+    function data($args = null)
+    {
+        if (func_num_args() == 0) {
+            throw new WarpInvalidDataException();
+        }
+
+        if (func_num_args() > 1) {
+            $args = func_get_args();
+        }
+
+        return new USS($args);
+    }
+}
+
+class USS
+{
+    function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    function get($key = null, $default = null)
+    {
+        $array = $this->data;
+
+        if (is_null($key)) {
+            return $array;
+        }
+
+        if (isset($array[$key])) {
+            return $array[$key];
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (! is_array($array) || ! array_key_exists($segment, $array)) {
+                return $this->value($default);
+            }
+            $array = $array[$segment];
+        }
+
+        return $array;
+    }
+
+    function count()
+    {
+        return count($this->data);
+    }
+
+    function sum($key = '')
+    {
+        if ($key == '') {
+            return array_sum($this->data);
+        }
+
+        return array_reduce($this->data, function ($carry, $item) use ($key) {
+            $carry += $item[$key];
+            return $carry;
+        });
+    }
+
+    public function map($callback = null)
+    {
+        return array_map($callback, $this->data);
+    }
+
+    public function filter($callback = null)
+    {
+        if ($callback == null) {
+            return array_filter($this->data);
+        }
+
+        return array_filter($this->data, $callback);
+    }
+
+    function value($value)
+    {
+        return $value instanceof Closure ? $value() : $value;
+    }
+}
+
+
+class WarpInvalidDataException extends \Exception {}
--- tests/WarpTest.php
+++ tests/WarpTest.php
@@ -2,7 +2,158 @@
 
 namespace Tests;
 
-class WarpTest
-{
+use Warp\Warp;
+use PHPUnit\Framework\TestCase;
+use Warp\WarpInvalidDataException;
 
+class WarpTest extends TestCase
+{
+    /** @test */
+    function it_can_create_a_warp_instance()
+    {
+        $this->assertInstanceOf(Warp::class, new Warp());
+    }
+
+    /** @test */
+    function a_array_of_data_can_be_set_to_prepare_the_drive()
+    {
+        $response = Warp::data([
+            1,
+            2,
+            3,
+        ])->get();
+
+        $this->assertArraySubset([1,2,3], $response);
+    }
+
+    /** @test */
+    function it_can_recive_many_arguments_an_return_as_array()
+    {
+        $response = Warp::data(1, 2, 3)->get();
+
+        $this->assertArraySubset([1,2,3], $response);
+    }
+
+    /** @test */
+    function it_can_count_an_array_of_data()
+    {
+        $count = Warp::data([1, 2, 3])->count();
+
+        $this->assertEquals(3, $count);
+    }
+
+    /** @test */
+    function it_can_count_many_arguments()
+    {
+        $count = Warp::data(1, 2, 3)->count();
+
+        $this->assertEquals(3, $count);
+    }
+
+    /** @test */
+    function it_can_sum_an_array_of_data()
+    {
+        $sum = Warp::data([1, 2, 3])->sum();
+
+        $this->assertEquals(6, $sum);
+    }
+
+    /** @test */
+    function it_can_sum_many_arguments()
+    {
+        $sum = Warp::data(1, 2, 3)->sum();
+
+        $this->assertEquals(6, $sum);
+    }
+
+    /** @test */
+    function it_can_sum_a_key_from_array_of_data()
+    {
+        $item = Warp::data([
+            ['item' => 1, 'amount' => 100],
+            ['item' => 3, 'amount' => 588],
+        ])->sum('item');
+
+        $amount = Warp::data([
+            ['item' => 1, 'amount' => 100],
+            ['item' => 3, 'amount' => 588],
+        ])->sum('amount');
+
+
+        $this->assertEquals(4, $item);
+        $this->assertEquals(688, $amount);
+    }
+
+    /** @test */
+    function it_can_map_an_array_of_data()
+    {
+        $items = Warp::data([
+            ['item' => 1, 'amount' => 100],
+            ['item' => 3, 'amount' => 588],
+        ])->map(function ($items) {
+            return $items['item'];
+        });
+
+        $amounts = Warp::data([
+            ['item' => 1, 'amount' => 100],
+            ['item' => 3, 'amount' => 588],
+        ])->map(function ($items) {
+            return $items['amount'];
+        });
+
+        $this->assertArraySubset([1, 3], $items);
+        $this->assertArraySubset([100, 588], $amounts);
+    }
+
+    /** @test */
+    function it_can_filter_an_array_of_data()
+    {
+        $items = Warp::data([
+            ['item' => 1, 'amount' => 100],
+            ['item' => 3, 'amount' => 588],
+        ])->filter(function ($items) {
+            return $items['item'] > 1;
+        });
+
+        $this->assertArraySubset([
+            1 => ['item' => 3, 'amount' => 588]
+        ], $items);
+    }
+
+    /** @test */
+    function it_can_filter_false_null_empty_string_from_array_of_data()
+    {
+        $filter = Warp::data(['foo', false, -1, null, ''])->filter();
+
+        $this->assertArraySubset([0 => 'foo', 2 => -1], $filter);
+    }
+
+    /** @test */
+    function it_can_get_an_element_form_data()
+    {
+        $data = [
+            'user' => 'Wilman',
+            'item' => [
+                'name' => 'Ipod',
+                'amount' => 250
+            ]
+        ];
+
+        $user = Warp::data($data)->get('user');
+        $name = Warp::data($data)->get('item.name');
+        $amount = Warp::data($data)->get('item.amount');
+
+        $this->assertEquals($user, 'Wilman');
+        $this->assertEquals($name, 'Ipod');
+        $this->assertEquals($amount, 250);
+    }
+
+    /**
+     * @test
+     * @expectedException \Warp\WarpInvalidDataException
+     */
+    function it_can_thrown_an_exception_if_data_is_not_pass()
+    {
+        Warp::data();
+    }
 }
